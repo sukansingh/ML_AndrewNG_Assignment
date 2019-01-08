9f144668ca953d294e77891ad1caba3ff9b39c62
Sukan Singh <sukansingh@gmail.com>
2019-01-09 00:39:38 +0530

    added code to the RandomIntitializeWeight and refactored nnCostFunction

--- machine-learning-ex4/ex4/nnCostFunction_Loop.m
+++ machine-learning-ex4/ex4/nnCostFunction_Loop.m
@@ -64,64 +64,37 @@ Theta2_grad = zeros(size(Theta2));
 
 % ================================= Non-Vectorized(Loop) Implementation ========================================
 
-%D2 = zeros(num_labels, hidden_layer_size+1); % 10*26
-%D1 = zeros(hidden_layer_size, input_layer_size+1); % 25*401
-%
-%Y = zeros(m,num_labels) ; % 5000X10 matrix of zeros
-% 
-%c = 1:num_labels;
-%Y = y==c;
-%
-%h = zeros(m,num_labels);
-%for i = 1:m
-%	a1 = [1 X(i,:)]; % 1*401
-%	z2 = a1*Theta1'; %(1*401)*(401*25)
-%	a2 = [1 sigmoid(z2)]; % 1*26
-%	
-%	z3 = a2*Theta2';
-%	a3 = h(i,:) = sigmoid(z3);  % 1*10
-%	
-%	d3 = a3-Y(i,:);	% 1*10
-%	d2 = (d3*Theta2) .* [1 sigmoidGradient(z2)];  % (1*10) * (10*26)
-%	d2_without_bias = d2(2:end);
-%	
-%	D2 = D2 + d3'*a2;	% (1*10)' * (1*26)
-%	D1 = D1 + d2_without_bias'*a1;	% (1*25)' * (1*401)
-%	
-%end;
-%J = sum(sum(((-Y.*log(h)) - ((1-Y).*log(1-h)))/m, 2));
-%
-%J += (lambda/(2*m))*(sum(sum(Theta1(:,2:end).^2))+sum(sum(Theta2(:,2:end).^2)));
-%
-%Theta2_grad = D2/m; % 10*26
-%Theta1_grad = D1/m; % 25*401
+D2 = zeros(num_labels, hidden_layer_size+1); % 10*26
+D1 = zeros(hidden_layer_size, input_layer_size+1); % 25*401
 
-% ================================= Vectorized Implementation ========================================
-
-X = [ones(m,1) X]; % m*401
-z2 = X*Theta1';
-a2 = sigmoid(z2); % m*25
-a2 = [ones(m,1) a2];
-z3 = a2*Theta2';
-a3 = h = sigmoid(z3);  % m*10
-
+Y = zeros(m,num_labels) ; % 5000X10 matrix of zeros
+ 
 c = 1:num_labels;
-y = y==c;
+Y = y==c;
 
-D3 = h-y; % m*10
-D2 = (D3*Theta2); % m*26
-%D2 = (D2 .* a2).*(1-a2);
-D2 = D2 .* [ones(m,1) sigmoidGradient(z2)];
-
-D2 = D2(:,2:end); % m*25
-
-Theta2_grad = (D3'*a2)/m; % 10*26
-Theta1_grad = (D2'*X)/m; % 25*401
+h = zeros(m,num_labels);
+for i = 1:m
+	a1 = [1 X(i,:)]; % 1*401
+	z2 = a1*Theta1'; %(1*401)*(401*25)
+	a2 = [1 sigmoid(z2)]; % 1*26
 	
-J = sum(sum(((-y.*log(h)) - ((1-y).*log(1-h)))/m, 2));
-%J = sum(sum(((-y.*log(h)) - ((1-y).*log(1-h)))/m));
+	z3 = a2*Theta2';
+	a3 = h(i,:) = sigmoid(z3);  % 1*10
+	
+	d3 = a3-Y(i,:);	% 1*10
+	d2 = (d3*Theta2) .* [1 sigmoidGradient(z2)];  % (1*10) * (10*26)
+	d2_without_bias = d2(2:end);
+	
+	D2 = D2 + d3'*a2;	% (1*10)' * (1*26)
+	D1 = D1 + d2_without_bias'*a1;	% (1*25)' * (1*401)
+	
+end;
+J = sum(sum(((-Y.*log(h)) - ((1-Y).*log(1-h)))/m, 2));
+
 J += (lambda/(2*m))*(sum(sum(Theta1(:,2:end).^2))+sum(sum(Theta2(:,2:end).^2)));
 
+Theta2_grad = D2/m; % 10*26
+Theta1_grad = D1/m; % 25*401
 
 % =========================================================================
 
